b080f5f351dfa08083f6cc93d7fb82f95798d9e3
santosh <styadav018@gmail.com>
2023-12-01 02:08:30 +0530

    add time key in links

--- models/Project.model.js
+++ models/Project.model.js
@@ -15,6 +15,10 @@ const projectSchema = mongoose.Schema({
         type: String,
         required: true,
       },
+      time: {
+        type: String,
+        required: true,
+      },
     },
   ],
 });
